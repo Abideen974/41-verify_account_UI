4cc4381dcfd956f6cca5630fc993317d572c4908
Abideen <Abideenali926@gmail.com>
2022-04-21 00:07:48 -0700

    verify account UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Verifyaccount from './components/Verifyaccount'
+
+function App() {
+  return (
+   <Verifyaccount />
+  );
+}
+
+export default App;
+
+
+// netlify Url https://app.netlify.com/sites/verify-account-ui-41/settings/domain
---
+++ src/components/Verifyaccount.jsx
@@ -0,0 +1,54 @@
+import React,{useEffect} from 'react'
+import './verfiy.css';
+function Verifyaccount() {
+    useEffect(()=>{
+        const codes = document.querySelectorAll('.code')
+    
+    
+    codes[0]?.focus()
+    
+    codes.forEach((code, idx) => {
+        code.addEventListener('keydown', (e) => {
+            if (e.key >= 0 && e.key <= 9) {
+                codes[idx].value = ''
+                setTimeout(() => codes[idx + 1].focus(), 10)
+                if(idx == 5) {
+                    idx = 0;
+                }
+            } else if (e.key === 'Backspace') {
+                setTimeout(() => codes[idx - 1].focus(), 10)
+                if(idx == 0) {
+                    idx = 5;
+                }
+            }
+        })
+    })
+    
+    },[])
+    
+      return (
+        
+        <div className="container">
+            <h2>Verify your Account</h2>
+            <p>
+                We emailed you the six digit code to cool_guy@gmail.com <br />Enter the code below to confirm your email address.
+            </p>
+            <div className="code-container">
+                
+            <input type="number" className="code" min="0" max="9" placeholder='0' required/>
+            <input type="number" className="code" min="0" max="9" placeholder='0' required/>
+            <input type="number" className="code" min="0" max="9" placeholder='0' required/>
+            <input type="number" className="code" min="0" max="9" placeholder='0' required/>
+            <input type="number" className="code" min="0" max="9" placeholder='0' required/>
+            <input type="number" className="code" min="0" max="9" placeholder='0' required/>
+            </div>
+            <small className="info">
+                This is design only. We didn't actually send
+                you an email as we don't have your email, right?
+            </small>
+        </div>
+    
+      )
+    }
+
+export default Verifyaccount
